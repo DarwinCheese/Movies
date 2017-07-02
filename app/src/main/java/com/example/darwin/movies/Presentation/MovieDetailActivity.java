7f59195bda1da2c996c61b8951e6f081dc510cd4
Darwin Gutierrez <darwincheese@gmail.com>
2017-07-03 01:48:49 +0200

    First commit

---
+++ app/src/main/java/com/example/darwin/movies/Presentation/MovieDetailActivity.java
@@ -0,0 +1,8 @@
+package com.example.darwin.movies.Presentation;
+
+/**
+ * Created by Darwin on 3-7-2017.
+ */
+
+public class MovieDetailActivity {
+}
---
+++ app/src/main/java/com/example/darwin/movies/Service/Config.java
@@ -0,0 +1,14 @@
+package com.example.darwin.movies.Service;
+
+/**
+ * Created by Darwin on 3-7-2017.
+ */
+
+public class Config {
+
+    public static final String URL_LOGIN ="https://node-movielibrary.herokuapp.com/api/v1/login";
+    public static final String URL_REGISTER ="https://node-movielibrary.herokuapp.com/api/v1/register";
+    public static final String URL_FILMS ="https://node-movielibrary.herokuapp.com/api/v1/films?offset=0&count=5";
+    public static final String URL_RENTALS ="https://node-movielibrary.herokuapp.com/api/v1/rentals";
+
+}
